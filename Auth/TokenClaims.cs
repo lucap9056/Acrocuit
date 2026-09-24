fc0996959196e30641b9b6a60c1e5b86d868cb80
lucap9056 <jim12366219@gmail.com>
2026-09-24 17:20:53 +0800

    refactor: Remove .NET + MSSQL implementation

--- Auth/TokenClaims.cs
+++
@@ -1,8 +0,0 @@
-namespace Acrocuit.Auth;
-
-public static class TokenClaims
-{
-    public const string TokenUse = "token_use";
-    public const string AccessTokenUse = "access";
-    public const string RefreshTokenUse = "refresh";
-}
